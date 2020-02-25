4c8f0a4462016f280ed7e0c62a678d6ad6be4ec6
Michael Reynolds <michael.reynolds@twosixlabs.com>
2020-02-25 18:59:20 -0500

    update the module info with the actual project info

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY *.py /opt/app/interstellar/
 COPY interstellar/* /opt/app/interstellar/interstellar/
 COPY Makefile /opt/app/interstellar
 COPY requirements.txt /opt/app/interstellar
-COPY README.rst /opt/app/interstellar
+COPY README.md /opt/app/interstellar
 COPY LICENSE /opt/app/interstellar
 
 RUN cd /opt/app/interstellar && \
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
 from setuptools import setup, find_packages
 
-
-with open('README.rst') as f:
+with open( 'README.md' ) as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open( 'LICENSE' ) as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    name = 'interstellar',
+    version = '0.1.0',
+    description = 'Dockerized script for pre-deploying Kafka topics',
+    long_description = readme,
+    author = 'Michael Reynolds',
+    author_email = 'reynoldsm88@gmail.com',
+    url = 'https://github.com/reynoldsm88/interstellar',
+    license = license,
+    packages = find_packages( exclude = ('tests', 'docs') )
 )
